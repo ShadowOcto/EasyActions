f22df2166fb597f6c4a1b9f9b24a2d693e9883cf
Shadow <80975779+ShadowOcto@users.noreply.github.com>
2023-01-10 00:31:16 +0000

    Add files via upload

---
+++ Dependencies/Optimize.bat
@@ -0,0 +1,22 @@
+@echo off
+title [EasyActions] Ram Optimizer
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+cls
+echo Optimizing Ram...
+EmptyStandbyList.exe workingsets
+EmptyStandbyList.exe modifiedpagelist
+EmptyStandbyList.exe priority0standbylist
+EmptyStandbyList.exe standbylist
